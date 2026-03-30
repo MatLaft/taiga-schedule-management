1d7ad86a3ce0ee196aaf889c6945a982a4f7f1cb
Matheus Ed. <matheus.lafeta.moc@gmail.com>
2026-03-30 17:54:14 -0300

    updating docker to use local

--- start-taiga-local.sh
+++ start-taiga-local.sh
@@ -98,8 +98,8 @@ export ATTACHMENTS_MAX_AGE="${ATTACHMENTS_MAX_AGE:-360}"
 echo "[2/7] Parando e removendo containers atuais..."
 compose_base down --remove-orphans
 
-echo "[3/7] Gerando imagens locais (back/async/manage)..."
-compose_with_inits build taiga-back taiga-async taiga-manage
+echo "[3/7] Gerando imagens locais (back/async)..."
+compose_base build --no-cache taiga-back taiga-async
 
 echo "[4/7] Subindo servicos base do Taiga..."
 compose_base up -d \
